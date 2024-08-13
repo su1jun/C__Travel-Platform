bcef9abcd8759acba026acd5fce316cef5a7a637
su1jun <qaxsd530@gmail.com>
2024-08-13 21:29:07 +0900

    feat: nav에 container 선언

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 const Navbar = () => {
   return ( 
-    <div>
-      I am a Navbar!.
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
+      </div>
     </div>
   );
 }
-
 
 export default Navbar;
