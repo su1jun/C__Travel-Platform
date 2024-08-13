e1176de7a9ebcf61e29776df8d0ef142766c20d4
su1jun <qaxsd530@gmail.com>
2024-08-13 21:50:36 +0900

    fix: 메뉴바 레이아웃 수정

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,9 +1,17 @@
 'use client';
 
+import { useCallback, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
+
 import Avatar from "../Avatar";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+  
   return ( 
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -25,7 +33,7 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div 
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="
             p-4
             md:py-1
@@ -48,6 +56,28 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div 
+          className="
+            absolute 
+            rounded-xl 
+            shadow-md
+            w-[40vw]
+            md:w-3/4 
+            bg-white 
+            overflow-hidden 
+            right-0 
+            top-12 
+            text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+              <>
+                <MenuItem />
+              </>
+          </div>
+        </div>
+      )}
     </div>
    );
 }
