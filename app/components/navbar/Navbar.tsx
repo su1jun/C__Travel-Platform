f0b104fc432dec923529b319e86d2d215c6d6373
su1jun <qaxsd530@gmail.com>
2024-08-13 21:35:21 +0900

    feat: nav에 logo 컴포넌트 추가, container 구체화

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Container from "../Container";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return ( 
@@ -10,7 +11,18 @@ const Navbar = () => {
         "
       >
       <Container>
-        Anything
+        <div 
+          className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+        >
+          <Logo />
+        </div>
       </Container>
       </div>
     </div>
