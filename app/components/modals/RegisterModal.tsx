ffd08724fbf0e88619a45d076268f0437cb06876
su1jun <qaxsd530@gmail.com>
2024-08-15 20:13:04 +0900

    feat: register 모달 footer 추가

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -16,6 +16,7 @@ import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
 import Input from "../inputs/Input";
 import Heading from "../Heading";
+import Button from "../Button";
 
 const RegisterModal= () => {
   const registerModal = useRegisterModal();
@@ -84,6 +85,45 @@ const RegisterModal= () => {
       />
     </div>
   )
+  
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button 
+        outline 
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}} 
+      />
+      <Button 
+        outline 
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => ()}
+      />
+      <div 
+        className="
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        "
+      >
+        <p>Already have an account?
+          <span 
+            onClick={registerModal.onClose} 
+            className="
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            "
+          > 
+            Log in
+          </span>
+        </p>
+      </div>
+    </div>
+  )
 
   return (
     <Modal
@@ -94,6 +134,7 @@ const RegisterModal= () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 }
