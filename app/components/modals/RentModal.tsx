421cd3d187cbff05af12faafc5ab8f970e5c5c8c
su1jun <qaxsd530@gmail.com>
2024-09-04 23:39:15 +0900

    fix: RentModal 상태관리 로직 추가

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,20 +1,57 @@
 'use client';
+import { useMemo, useState } from "react";
 
 import useRentModal from '@/app/hooks/useRentModal';
 
 import Modal from "./Modal";
 
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
   const rentModal = useRentModal();
 
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  }
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  }
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return 'Create'
+    }
+
+    return 'Next'
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined
+    }
+
+    return 'Back'
+  }, [step]);
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
-      onSubmit={rentModal.onClose}
       onClose={rentModal.onClose}
+      onSubmit={rentModal.onClose}
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
       title="Airbnb your home!"
-      actionLabel="Submit"
     />
   );
 }
