208e6db19880d9ef9d1dae1af304f68da6cbe0ed
su1jun <qaxsd530@gmail.com>
2024-09-05 23:21:23 +0900

    feat: rent 모달에 나라 선택 기능 추가

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import useRentModal from '@/app/hooks/useRentModal';
 
 import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
+import CountrySelect from "../inputs/CountrySelect";
 import Modal from "./Modal";
 import Heading from '../Heading';
 
@@ -113,6 +114,19 @@ const RentModal = () => {
       </div>
     </div>
   )
+
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect 
+        />
+      </div>
+    );
+  }
 
   return (
     <Modal
